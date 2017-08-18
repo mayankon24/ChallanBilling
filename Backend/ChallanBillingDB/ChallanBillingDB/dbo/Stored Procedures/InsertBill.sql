b4e160bb74268f909a6ac789f6f75ed07ffe40bf
Mayank Aggarwal (HCL America INC) <v-maag@microsoft.com>
2017-08-17 18:19:06 -0700

    update database

--- Backend/ChallanBillingDB/ChallanBillingDB/dbo/Stored Procedures/InsertBill.sql
+++ Backend/ChallanBillingDB/ChallanBillingDB/dbo/Stored Procedures/InsertBill.sql
@@ -15,7 +15,7 @@
 AS
 	SET NOCOUNT OFF;
 	
-	Declare @Financial_Year NVARCHAR(50)
+	
 	Declare @StartFinancial_Year datetime
 	Declare @EndFinancial_Year datetime
 	Declare @Company_Type int
@@ -32,28 +32,17 @@ AS
 						when month(@Bill_Date) >= 4	then DATEADD(YEAR, year(@Bill_Date)-1900 +1, DATEADD(MONTH, 3, -1)) 
 						when month(@Bill_Date) < 4	then DATEADD(YEAR, year(@Bill_Date)-1900, DATEADD(MONTH, 3, -1))
 					 end
-	-- Get Financial Year
-	set @Financial_Year = case
-						when month(@Bill_Date) >= 4 then CONVERT(nvarchar, year(@Bill_Date)) +'-' +  CONVERT(nvarchar,  year(@Bill_Date)  + 1 ) + '/ '
-						else  CONVERT(nvarchar, year(@Bill_Date) - 1 ) +'-' +  CONVERT(nvarchar,  year(@Bill_Date) ) + '/ '
-					end 
+
 	
-	
-	
-	SELECT @BillNo_Code = case 
-						WHEN @Bill_Type_Id = 1 THEN ISNULL(max(BillNo_Code), 0) 
-						WHEN @Bill_Type_Id = 2 THEN ISNULL(max(BillNo_Code), 0) 
-						WHEN @Bill_Type_Id = 3 THEN ISNULL(max(BillNo_Code), 0) 
-						WHEN @Bill_Type_Id = 4 THEN ISNULL(max(BillNo_Code), 0)
-					END
+	SELECT @BillNo_Code = ISNULL(max(BillNo_Code), 0)
 	FROM B_BILL	INNER JOIN 
 	M_COMPANY ON  B_BILL.Company_id = M_COMPANY.Company_id
 	Where		
 	(
 		((@Bill_Type_Id = 1 or @Bill_Type_Id = 3 ) AND (Bill_Type_Id = 1 or Bill_Type_Id = 3))
 		OR ((@Bill_Type_Id = 4 or @Bill_Type_Id = 2 ) AND (Bill_Type_Id = 4 or Bill_Type_Id = 2))
-	)
-	And M_COMPANY.Company_Type_Id =@Company_Type
+	) And
+	 M_COMPANY.Company_Type_Id =@Company_Type
 	AND Bill_Date >= @StartFinancial_Year 
 	AND Bill_Date <= @EndFinancial_Year
 	
@@ -70,15 +59,7 @@ AS
 	--get Bill No 
 	Declare @BillNo  NVARCHAR(50)
 	select @BillNo = CONVERT(nvarchar,  @BillNo_Code)
-	--SELECT @BillNo = case 
-	--						 WHEN @Bill_Type_Id = 1 THEN 'RIS ' + @Financial_Year + CONVERT(nvarchar,  @BillNo_Code)
-	--					       WHEN @Bill_Type_Id = 2 THEN 'TIS ' + @Financial_Year + CONVERT(nvarchar,  @BillNo_Code)
-	--						 WHEN @Bill_Type_Id = 3 THEN 'RIJ ' + @Financial_Year + CONVERT(nvarchar,  @BillNo_Code)
-	--						 WHEN @Bill_Type_Id = 4 THEN 'TIJ ' + @Financial_Year + CONVERT(nvarchar,  @BillNo_Code)
-	--					  END
-
-
-
+	
     if(@Bill_No is not null AND @Bill_No  <> '' and @Bill_No  <> 0)		
 	begin
 		set @BillNo = @Bill_No
